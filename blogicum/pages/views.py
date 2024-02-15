761e11b309654c64719c6e3416639d943f372504
Nektare <nektare-m@gmail.com>
2024-02-15 17:05:19 +1000

    hw

---
+++ blogicum/pages/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+
+def about(request):
+
+    template_name = 'pages/about.html'
+
+    return render(request, template_name)
+
+
+def rules(request):
+
+    template_name = 'pages/rules.html'
+
+    return render(request, template_name)
